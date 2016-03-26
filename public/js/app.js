eec93b7d132ad1237078b893ce43b5261d50ace8
Jack Zhang <15jzhang@gmail.com>
2016-03-26 18:26:33 -0400

    Update display with new time digits

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,45 @@ function updateDisplay() {
 
 function updateColumn(unit, column, newValue) {
     var squareIdPrefix = unit + '-' + column + '-';
-    
+
+    if ( newValue/8 >= 1 ) {
+        updateSquare(squareIdPrefix + '8', true);
+        newValue = newValue % 8;
+    } else {
+        updateSquare(squareIdPrefix + '8', false);
+    }
+
+    if ( newValue/4 >= 1 ) {
+        updateSquare(squareIdPrefix + '4', true);
+        newValue = newValue % 4;
+    } else {
+        updateSquare(squareIdPrefix + '4', false);
+    }
+
+    if ( newValue/2 >= 1 ) {
+        updateSquare(squareIdPrefix + '2', true);
+        newValue = newValue % 2;
+    } else {
+        updateSquare(squareIdPrefix + '2', false);
+    }
+
+    if ( newValue/1 == 1 ) {
+        updateSquare(squareIdPrefix + '1', true);
+    } else {
+        updateSquare(squareIdPrefix + '1', false);
+    }
+}
+
+function updateSquare(squareId, turnOn) {
+    if ( $('#' + squareId).hasClass('digit-none') == false ) {
+        if ( turnOn ) {
+            $('#' + squareId).removeClass('digit-0');
+            $('#' + squareId).addClass('digit-1');
+        } else {
+            $('#' + squareId).removeClass('digit-1');
+            $('#' + squareId).addClass('digit-0');
+        }
+    }
 }
 
 function formatTime() {
@@ -32,11 +70,11 @@ function formatTime() {
     var timeDigits = new Array();
 
     // Add padding and convert to type string
-    for (var i = 0; i < time.length; i++) {
+    for ( var i = 0; i < time.length; i++ ) {
         time[i] = time[i] > 9 ? "" + time[i]: "0" + time[i];
 
         // Append individual digits to timeDigits 
-        for (var j = 0; j < time[i].length; j++) {
+        for ( var j = 0; j < time[i].length; j++ ) {
             var timeDigit = Number(time[i][j]);
             timeDigits.push(timeDigit);
         }
