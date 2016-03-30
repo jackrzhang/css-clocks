8612b7c228e4f9d49e9a698d4ad4a49aeceee019
Jack Zhang <15jzhang@gmail.com>
2016-03-29 23:36:36 -0400

    Add clock-info-time

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var timeDigits = formatTime();
 
     tickTock();
+    clockInfo();
 });
 
 function tickTock() {
@@ -38,6 +39,38 @@ function updateDisplay() {
     if ( previousTimeDigits[5] != timeDigits[5] ) 
         updateDomino('seconds', 'ones', timeDigits[5]);
 }
+
+function clockInfo() {
+    // Update clock info display
+    setInterval( function() {
+        var time = getCurrentTime();
+
+        for ( var i = 0; i < time.length; i++ ) {
+            time[i] = time[i] > 9 ? "" + time[i]: "0" + time[i];
+        }
+
+        $('#info-time-hours').html(time[0]);
+        $('#info-time-minutes').html(time[1]);
+        $('#info-time-seconds').html(time[2]);
+    }, 1000)
+
+    // Binary clock to clock information animation
+    $('.clock-display').on('click', function() {
+        $('.clock-info-time').animate({ opacity: 1 }, 1000);
+        $('.clock-display').animate({ opacity: 0 }, 1000);
+        $('.clock-display').css('z-index', '0');
+        $('.clock-info-time').css('z-index', '10');
+    })
+
+    // Clock information to binary clock animation
+    $('.clock-info-time').on('click', function() {
+        $('.clock-display').animate({ opacity: 1 }, 1000);
+        $('.clock-info-time').animate({ opacity: 0 }, 1000);
+        $('.clock-info-time').css('z-index', '0');
+        $('.clock-display').css('z-index', '10');
+    })
+}
+
 
 function formatTime() {
     var time = getCurrentTime();
