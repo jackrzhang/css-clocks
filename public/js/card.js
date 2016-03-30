fc829e95271fce883bbde9bbfdcde2cb11b6d6fa
Jack Zhang <15jzhang@gmail.com>
2016-03-29 21:34:29 -0400

    Add JS - determine suit randomly, update card value

--- public/js/card.js
+++ public/js/card.js
@@ -1,7 +1,7 @@
 function updateCard(unit, column, value) {
     var cardId = unit + '-' + column;
 
-    // Determine card color (red/black) randomly
+    // Determine and update card color (red, black) randomly
     if ( Math.random() < 0.5 ) {
         $('#' + cardId).removeClass('black');
         $('#' + cardId).addClass('red');
@@ -9,50 +9,61 @@ function updateCard(unit, column, value) {
         $('#' + cardId).removeClass('red');
         $('#' + cardId).addClass('black');
     }
+
+    // Determine card suit randomly
+    var suitRandom = Math.random();
+    if ( Math.random() < 0.25 ) {
+        // spades
+        var suit = '&#9824';
+    } else if ( Math.random() < 0.5 ) {
+        // hearts
+        var suit = '&#9829';
+    } else if ( Math.random() < 0.75 ) {
+        // clubs
+        var suit = '&#9827';
+    } else {
+        // diamonds
+        var suit = '&#9830';
+    }
+
+    // Update card value
+    $('#' + cardId).children('.card-value').html(value);
+
+    // Update all suit symbols on card
+    //updateSuitSymbols(cardId, suit, value);
 }
 
 function updateSuitSymbols(cardId, suit, value) {
 
     switch(value) {
-        case 0:
-            $(halfDominoQuery).children('.dot-one').removeClass('show');
-            $(halfDominoQuery).children('.dot-two').removeClass('show');
-            $(halfDominoQuery).children('.dot-three').removeClass('show');
-            $(halfDominoQuery).children('.dot-four').removeClass('show');
-            $(halfDominoQuery).children('.dot-five').removeClass('show');
-            $(halfDominoQuery).children('.dot-six').removeClass('show');
-            $(halfDominoQuery).children('.dot-seven').removeClass('show');
-            $(halfDominoQuery).children('.dot-eight').removeClass('show');
-            $(halfDominoQuery).children('.dot-nine').removeClass('show');
-            break;
         case 1:
             $(halfDominoQuery).children('.dot-one').removeClass('show');
             $(halfDominoQuery).children('.dot-two').removeClass('show');
             $(halfDominoQuery).children('.dot-three').removeClass('show');
             $(halfDominoQuery).children('.dot-four').removeClass('show');
-            $(halfDominoQuery).children('.dot-five').addClass('show');
+            $(halfDominoQuery).children('.dot-five').removeClass('show');
             $(halfDominoQuery).children('.dot-six').removeClass('show');
             $(halfDominoQuery).children('.dot-seven').removeClass('show');
             $(halfDominoQuery).children('.dot-eight').removeClass('show');
             $(halfDominoQuery).children('.dot-nine').removeClass('show');
             break;
         case 2:
-            $(halfDominoQuery).children('.dot-one').addClass('show');
+            $(halfDominoQuery).children('.dot-one').removeClass('show');
             $(halfDominoQuery).children('.dot-two').removeClass('show');
             $(halfDominoQuery).children('.dot-three').removeClass('show');
             $(halfDominoQuery).children('.dot-four').removeClass('show');
-            $(halfDominoQuery).children('.dot-five').removeClass('show');
+            $(halfDominoQuery).children('.dot-five').addClass('show');
             $(halfDominoQuery).children('.dot-six').removeClass('show');
             $(halfDominoQuery).children('.dot-seven').removeClass('show');
             $(halfDominoQuery).children('.dot-eight').removeClass('show');
-            $(halfDominoQuery).children('.dot-nine').addClass('show');
+            $(halfDominoQuery).children('.dot-nine').removeClass('show');
             break;
         case 3:
             $(halfDominoQuery).children('.dot-one').addClass('show');
             $(halfDominoQuery).children('.dot-two').removeClass('show');
             $(halfDominoQuery).children('.dot-three').removeClass('show');
             $(halfDominoQuery).children('.dot-four').removeClass('show');
-            $(halfDominoQuery).children('.dot-five').addClass('show');
+            $(halfDominoQuery).children('.dot-five').removeClass('show');
             $(halfDominoQuery).children('.dot-six').removeClass('show');
             $(halfDominoQuery).children('.dot-seven').removeClass('show');
             $(halfDominoQuery).children('.dot-eight').removeClass('show');
@@ -61,6 +72,17 @@ function updateSuitSymbols(cardId, suit, value) {
         case 4:
             $(halfDominoQuery).children('.dot-one').addClass('show');
             $(halfDominoQuery).children('.dot-two').removeClass('show');
+            $(halfDominoQuery).children('.dot-three').removeClass('show');
+            $(halfDominoQuery).children('.dot-four').removeClass('show');
+            $(halfDominoQuery).children('.dot-five').addClass('show');
+            $(halfDominoQuery).children('.dot-six').removeClass('show');
+            $(halfDominoQuery).children('.dot-seven').removeClass('show');
+            $(halfDominoQuery).children('.dot-eight').removeClass('show');
+            $(halfDominoQuery).children('.dot-nine').addClass('show');
+            break;
+        case 5:
+            $(halfDominoQuery).children('.dot-one').addClass('show');
+            $(halfDominoQuery).children('.dot-two').removeClass('show');
             $(halfDominoQuery).children('.dot-three').addClass('show');
             $(halfDominoQuery).children('.dot-four').removeClass('show');
             $(halfDominoQuery).children('.dot-five').removeClass('show');
@@ -69,7 +91,7 @@ function updateSuitSymbols(cardId, suit, value) {
             $(halfDominoQuery).children('.dot-eight').removeClass('show');
             $(halfDominoQuery).children('.dot-nine').addClass('show');
             break;
-        case 5:
+        case 7:
             $(halfDominoQuery).children('.dot-one').addClass('show');
             $(halfDominoQuery).children('.dot-two').removeClass('show');
             $(halfDominoQuery).children('.dot-three').addClass('show');
@@ -80,7 +102,7 @@ function updateSuitSymbols(cardId, suit, value) {
             $(halfDominoQuery).children('.dot-eight').removeClass('show');
             $(halfDominoQuery).children('.dot-nine').addClass('show');
             break;
-        case 6:
+        case 8:
             $(halfDominoQuery).children('.dot-one').addClass('show');
             $(halfDominoQuery).children('.dot-two').removeClass('show');
             $(halfDominoQuery).children('.dot-three').addClass('show');
